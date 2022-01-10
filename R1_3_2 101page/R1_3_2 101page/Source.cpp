a8701e33bc876611eda2e35bd0b5ffb7effef3e1
DevKnow <helio524@naver.com>
2022-01-10 23:27:45 +0900

    3_2 101page 복습1회차
    3_2 101page 복습1회차

---
+++ R1_3_2 101page/R1_3_2 101page/Source.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int search(const int a[], int n, int key) {
+	for (int i = 0; i < n; i++) {
+		if (a[i] == key) return i; // 검색 성공
+	}
+	return -1; // 검색 실패
+}
+
+int main() {
+
+	int nx, ky, idx;
+	int* x;
+	puts("선형 검색");
+	printf("요소 개수: "); scanf("%d", &nx);
+	x = (int*)calloc(nx, sizeof(int));
+	for (int i = 0; i < nx; i++) {
+		printf("x[%d] = ", i); scanf("%d", &x[i]);
+	}
+	printf("\n검색하려는 값: "); scanf("%d", &ky);
+	idx = search(x, nx, ky);
+	if (idx == -1)
+		puts("검색 실패");
+	else
+		printf("%d는 x[%d]에 있습니다.", ky, idx);
+	free(x);
+
+	return 0;
+}

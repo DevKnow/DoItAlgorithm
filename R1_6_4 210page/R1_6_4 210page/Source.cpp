db948be9a47aba71fbd076667f2287be6368bb5a
DevKnow <helio524@naver.com>
2022-01-18 17:02:02 +0900

    6_4 210page 복습1회차
    6_4 210page 복습1회차

---
+++ R1_6_4 210page/R1_6_4 210page/Source.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#define swap(type, x, y)do {type t=x; x=y; y=t;}while(0)
+
+void selection(int a[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		int min = i;
+		for (int j = i + 1; j < n; j++) {
+			if (a[j] < a[min]) min = j;
+		}
+		swap(int, a[i], a[min]);
+	}
+}
+
+int main() {
+	int i, nx;
+	puts("단순 선택 정렬");
+	printf("요소 개수: "); scanf("%d", &nx);
+	int* x = (int*)malloc(sizeof(int) * nx);
+
+	for (i = 0; i < nx; i++) {
+		printf("x[%d]: ", i); scanf("%d", &x[i]);
+	}
+
+	selection(x, nx);
+
+	puts("오름차순으로 정렬했습니다.");
+	for (i = 0; i < nx; i++)
+		printf("x[%d]=%d\n", i, x[i]);
+
+	free(x);
+
+	return 0;
+}

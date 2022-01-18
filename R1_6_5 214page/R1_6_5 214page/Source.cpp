0b0f6f125c1af1222d8053424926d02f8d619cf5
DevKnow <helio524@naver.com>
2022-01-18 17:14:08 +0900

    6_5 214page 복습1회차
    6_5 214page 복습1회차

---
+++ R1_6_5 214page/R1_6_5 214page/Source.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void insertion(int a[], int n) {
+	int j;
+	for (int i = 1; i < n; i++) {
+		int temp = a[i];
+		for (j = i; j > 0 && a[j-1] > temp; j--)
+			a[j] = a[j - 1];
+		a[j] = temp;
+	}
+}
+
+int main() {
+	int i, nx;
+	puts("단순 삽입 정렬");
+	printf("요소 개수: "); scanf("%d", &nx);
+	int* x = (int*)malloc(sizeof(int) * nx);
+
+	for (i = 0; i < nx; i++) {
+		printf("x[%d]: ", i); scanf("%d", &x[i]);
+	}
+
+	insertion(x, nx);
+
+	puts("오름차순으로 정렬했습니다.");
+	for (i = 0; i < nx; i++)
+		printf("x[%d]=%d\n", i, x[i]);
+
+	free(x);
+
+	return 0;
+}

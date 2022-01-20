e1e309be76b1456acfb500cc730038bc520dace0
DevKnow <helio524@naver.com>
2022-01-20 23:51:45 +0900

    6_7 222page 복습1회차
    6_7 222page 복습1회차

---
+++ R1_6_7 222page/R1_6_7 222page/Source.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void Shell(int a[], int n);
+
+int main() {
+	int nx;
+	puts("셸 정렬 버전2");
+	printf("요소개수: "); scanf("%d", &nx);
+	int* x = (int*)malloc(sizeof(int) * nx);
+	for (int i = 0; i < nx; i++) {
+		printf("x[%d]: ", i); scanf("%d", &x[i]);
+	}
+	Shell(x, nx);
+	puts("오름차순으로 정렬 완료");
+	for (int i = 0; i < nx; i++)
+		printf("x[%d]: %d\n", i, x[i]);
+	free(x);
+	return 0;
+}
+
+void Shell(int a[], int n) {
+	int i, j, h;
+	for (h = 1; h < n / 9; h = h * 3 + 1)
+		;
+	for (; h > 0; h /= 3) {
+		for (i = h; i < n; i++) {
+			int temp = a[i];
+			for (j = i - h; j >= 0 && a[j] > temp; j -= h)
+				a[j + h] = a[j];
+			a[j + h] = temp;
+		}
+	}
+}

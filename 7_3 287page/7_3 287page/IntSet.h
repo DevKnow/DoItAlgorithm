dee286149aae57282a53c2e2343c859277c3b654
DevKnow <helio524@naver.com>
2022-03-22 21:04:46 +0900

    7_3 287page
    7_3 287page

---
+++ 7_3 287page/7_3 287page/IntSet.h
@@ -0,0 +1,37 @@
+#ifndef __INT_SET_H__
+#define __INT_SET_H__
+
+typedef struct {
+	int max, num;
+	int* set;
+}IntSet;
+
+int Initialize(IntSet* s, int max);
+
+int IsMember(const IntSet* s, int n);
+
+void Add(IntSet* s, int n);
+
+void Remove(IntSet* s, int n);
+
+int Capacity(const IntSet* s);
+
+int Size(const IntSet* s);
+
+void Assign(IntSet* s1, const IntSet* s2);
+
+int Equal(const IntSet* s1, const IntSet* s2);
+
+IntSet* Union(IntSet* s1, const IntSet* s2, const IntSet* s3);
+
+IntSet* Intersection(IntSet* s1, const IntSet* s2, const IntSet* s3);
+
+IntSet* Difference(IntSet* s1, const IntSet* s2, const IntSet* s3);
+
+void Print(const IntSet* s);
+
+void PrintLn(const IntSet* s);
+
+void Terminate(IntSet* s);
+
+#endif
